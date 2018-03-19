292cc6fe835f404d455c442f38b61a0b8ee747fa
Kaidriver <jirachiji@gmail.com>
2018-03-19 19:44:48 -0400

    im so proud of myself smh

--- main.js
+++ main.js
@@ -4,13 +4,27 @@ function searchFunction() {
   filter = input.value.toUpperCase();
   ul = document.getElementById('wrapper');
   li = ul.getElementsByTagName('li');
+
   for (i=0; i < li.length; i++) {
+
     a = li[i].getElementsByTagName('a')[0];
     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.visibility = "visible" ;
       li[i].style.display = "";
+
+
     } else {
       li[i].style.display = 'none';
+
     }
+
+  }
+
+
+}
+function enter () {
+  if (event.keyCode == 13) {
+    document.getElementById('functionSearch').click()
   }
 }
 
